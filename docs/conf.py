d839a8af77b6a7b8e3bd623b5b4a2043df3b478d
Taiga MITSUYUKI <mitsuyuki-taiga-my@ynu.ac.jp>
2021-01-09 18:20:04 +0900

    WIP: preparing for uploading Pypi

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ author = "Taiga MITSUYUKI"
 # The short X.Y version.
 version = "0.0"
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.0.2"
 
 
 # -- General configuration ---------------------------------------------------
