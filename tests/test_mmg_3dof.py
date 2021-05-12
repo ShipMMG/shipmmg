2e39d0996e048c430290a56f4de7db8da60d0e1e
Taiga MITSUYUKI <mitsuyuki-taiga-my@ynu.ac.jp>
2021-05-12 16:28:01 +0900

    add wP formula 16

--- tests/test_mmg_3dof.py
+++ tests/test_mmg_3dof.py
@@ -5,6 +5,7 @@ from shipmmg.mmg_3dof import (
     Mmg3DofBasicParams,
     Mmg3DofManeuveringParams,
     simulate_mmg_3dof,
+    get_sub_values_from_simulation_result,
     zigzag_test_mmg_3dof,
 )
 from shipmmg.ship_obj_3dof import ShipObj3dof
@@ -37,6 +38,7 @@ def ship_KVLCC2_L7_model():
     a_H = 0.312  # 舵力増加係数
     x_H_dash = -0.464  # 舵力増分作用位置
     γ_R = 0.395  # 整流係数
+    # γ_R = 0.640  # 整流係数
     l_r_dash = -0.710  # 船長に対する舵位置
     x_P_dash = -0.650  # 船長に対するプロペラ位置
     ϵ = 1.09  # プロペラ・舵位置伴流係数比
@@ -143,12 +145,73 @@ def kvlcc2_L7_35_turning(ship_KVLCC2_L7_model):
     sim_result = sol.sol(time_list)
     ship = ShipObj3dof(L=basic_params.L_pp, B=basic_params.B)
     ship.load_simulation_result(time_list, sim_result[0], sim_result[1], sim_result[2])
+    ship.npm = npm_list
+    ship.δ = δ_rad_list
     return ship
 
 
-def test_Ship3DOF_drawing_function(kvlcc2_L7_35_turning):
-    """Check drawing functions of Ship3DOF class by using MMG 3DOF simulation results"""
+def test_get_sub_values_from_simulation_result(
+    kvlcc2_L7_35_turning, ship_KVLCC2_L7_model
+):
+    basic_params, maneuvering_params = ship_KVLCC2_L7_model
+    (
+        X_H_list,
+        X_R_list,
+        X_P_list,
+        Y_H_list,
+        Y_R_list,
+        N_H_list,
+        N_R_list,
+    ) = get_sub_values_from_simulation_result(
+        kvlcc2_L7_35_turning.u,
+        kvlcc2_L7_35_turning.v,
+        kvlcc2_L7_35_turning.r,
+        kvlcc2_L7_35_turning.δ,
+        kvlcc2_L7_35_turning.npm,
+        basic_params,
+        maneuvering_params,
+    )
+    (
+        X_H_list,
+        X_R_list,
+        X_P_list,
+        Y_H_list,
+        Y_R_list,
+        N_H_list,
+        N_R_list,
+        U_list,
+        β_list,
+        v_dash_list,
+        r_dash_list,
+        w_P_list,
+        J_list,
+        K_T_list,
+        v_R_list,
+        u_R_list,
+        U_R_list,
+        α_R_list,
+        F_N_list,
+    ) = get_sub_values_from_simulation_result(
+        kvlcc2_L7_35_turning.u,
+        kvlcc2_L7_35_turning.v,
+        kvlcc2_L7_35_turning.r,
+        kvlcc2_L7_35_turning.δ,
+        kvlcc2_L7_35_turning.npm,
+        basic_params,
+        maneuvering_params,
+        return_all_vals=True,
+    )
 
+    save_fig_path = "testFN.png"
+    fig = plt.figure()
+    plt.plot(kvlcc2_L7_35_turning.time, F_N_list)
+    fig.savefig(save_fig_path)
+    if os.path.exists(save_fig_path):
+        os.remove(save_fig_path)
+
+
+def test_Ship3DOF_drawing_function(kvlcc2_L7_35_turning):
+    """Check drawing functions of Ship3DOF class by using MMG 3DOF simulation results."""
     # Ship3DOF.draw_xy_trajectory()
     save_fig_path = "trajectory.png"
     kvlcc2_L7_35_turning.draw_xy_trajectory(dimensionless=True)
@@ -263,8 +326,8 @@ def test_zigzag_test_mmg_before(ship_KVLCC2_L7_model):
 def test_zigzag_test_mmg(ship_KVLCC2_L7_model):
 
     basic_params, maneuvering_params = ship_KVLCC2_L7_model
-    target_δ_rad = 20.0 * np.pi / 180.0
-    target_ψ_rad_deviation = -20.0 * np.pi / 180.0
+    target_δ_rad = 10.0 * np.pi / 180.0
+    target_ψ_rad_deviation = -10.0 * np.pi / 180.0
     duration = 100
     num_of_sampling = 10000
     time_list = np.linspace(0.00, duration, num_of_sampling)
@@ -278,7 +341,8 @@ def test_zigzag_test_mmg(ship_KVLCC2_L7_model):
         target_ψ_rad_deviation,
         time_list,
         npm_list,
-        δ_rad_rate=10.0 * np.pi / 180,
+        u0=1.18,
+        δ_rad_rate=15.0 * np.pi / 180,
     )
 
     ship = ShipObj3dof(L=100, B=10)
@@ -290,4 +354,94 @@ def test_zigzag_test_mmg(ship_KVLCC2_L7_model):
     fig = plt.figure()
     plt.plot(time_list, list(map(lambda δ: δ * 180 / np.pi, ship.δ)))
     plt.plot(time_list, list(map(lambda psi: psi * 180 / np.pi, ship.psi)))
-    plt.savefig(save_fig_path)
+    fig.savefig(save_fig_path)
+    plt.close()
+    if os.path.exists(save_fig_path):
+        os.remove(save_fig_path)
+
+    (
+        X_H_list,
+        X_R_list,
+        X_P_list,
+        Y_H_list,
+        Y_R_list,
+        N_H_list,
+        N_R_list,
+        U_list,
+        β_list,
+        v_dash_list,
+        r_dash_list,
+        w_P_list,
+        J_list,
+        K_T_list,
+        v_R_list,
+        u_R_list,
+        U_R_list,
+        α_R_list,
+        F_N_list,
+    ) = get_sub_values_from_simulation_result(
+        ship.u,
+        ship.v,
+        ship.r,
+        ship.δ,
+        ship.npm,
+        basic_params,
+        maneuvering_params,
+        return_all_vals=True,
+    )
+
+    save_fig_path = "w_P.png"
+    fig = plt.figure()
+    plt.plot(time_list, w_P_list)
+    fig.savefig(save_fig_path)
+    plt.close()
+    if os.path.exists(save_fig_path):
+        os.remove(save_fig_path)
+
+    save_fig_path = "J.png"
+    fig = plt.figure()
+    plt.plot(time_list, J_list)
+    fig.savefig(save_fig_path)
+    plt.close()
+    if os.path.exists(save_fig_path):
+        os.remove(save_fig_path)
+
+    save_fig_path = "K_T.png"
+    fig = plt.figure()
+    plt.plot(time_list, K_T_list)
+    fig.savefig(save_fig_path)
+    plt.close()
+    if os.path.exists(save_fig_path):
+        os.remove(save_fig_path)
+
+    save_fig_path = "U_R.png"
+    fig = plt.figure()
+    plt.plot(time_list, U_R_list)
+    fig.savefig(save_fig_path)
+    plt.close()
+    if os.path.exists(save_fig_path):
+        os.remove(save_fig_path)
+
+    save_fig_path = "α_R.png"
+    fig = plt.figure()
+    plt.plot(time_list, α_R_list)
+    fig.savefig(save_fig_path)
+    plt.close()
+    if os.path.exists(save_fig_path):
+        os.remove(save_fig_path)
+
+    save_fig_path = "F_N.png"
+    fig = plt.figure()
+    plt.plot(time_list, F_N_list)
+    fig.savefig(save_fig_path)
+    plt.close()
+    if os.path.exists(save_fig_path):
+        os.remove(save_fig_path)
+
+    save_fig_path = "F_N.png"
+    fig = plt.figure()
+    plt.plot(time_list, F_N_list)
+    fig.savefig(save_fig_path)
+    plt.close()
+    if os.path.exists(save_fig_path):
+        os.remove(save_fig_path)
